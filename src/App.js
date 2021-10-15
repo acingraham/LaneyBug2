4d790f1ee6811ec03930b59bf4b2a88ec7313ba1
Andrew Ingraham <acingraham@gmail.com>
2021-10-15 00:44:48 -0400

    Add react-router and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { Carousel } from 'react-responsive-carousel';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './App.css';
 
@@ -61,6 +65,7 @@ function useLocalStorage(name) {
   };
 }
 
+/*
 function Admin({list}) {
   const [index, setIndex] = useState(0);
   const addToLaney = useLocalStorage('laney');
@@ -118,7 +123,120 @@ function Admin({list}) {
     </div>
   );
 }
+*/
 
+function Admin() {
+  const [index, setIndex] = useState(0);
+  const addToLaney = useLocalStorage('laney');
+  const addToSkip = useLocalStorage('skip');
+  const addToOther = useLocalStorage('other');
+
+  // TODO - Move this to a separate hook
+  const [videoList, setVideoList] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('videoList.json');
+      const json = await response.json();
+      setVideoList(json);
+    }
+    fetchData();
+  }, []);
+
+  if (!videoList.length) {
+    return null;
+  }
+
+  const laneyBug = () => {
+    addToLaney(videoList[index]);
+    setIndex(prev => prev + 1);
+  };
+  const skip = () => {
+    addToSkip(videoList[index]);
+    setIndex(prev => prev + 1);
+  };
+  const back = () => {
+    setIndex(prev => prev - 1);
+  };
+  const other = () => {
+    addToOther(videoList[index]);
+    setIndex(prev => prev + 1);
+  };
+  const onKeyPress = event => {
+    if (event.key === ' ') {
+      laneyBug();
+    } else if (event.key === 'o') {
+      other();
+    } else if (event.key === 'b') {
+      back();
+    }
+  };
+  const onError = (err, url) => {
+    if (err.type === 'error') {
+      skip();
+    }
+  };
+
+  return (
+    <div onKeyPress={onKeyPress}>
+      <div className="button-section">
+        <button onClick={laneyBug}>LaneyBug</button>
+        <button onClick={skip}>Skip</button>
+        <button onClick={back}>Back</button>
+        <button onClick={other}>Other</button>
+      </div>
+      <ReactPlayer
+        url={`https://laneybug.s3.amazonaws.com/videos/${videoList[index]}`}
+        controls
+        loop
+        height="auto"
+        width="auto"
+        playing={true}
+        onError={err => onError(err, videoList[index])}
+        className="admin-player"
+      />
+    </div>
+  );
+}
+
+function View({file}) {
+  const [videoList, setVideoList] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      // const response = await fetch('videoList.json');
+      const response = await fetch(file);
+      const json = await response.json();
+      setVideoList(json);
+    }
+    fetchData();
+  }, [file]);
+
+  if (!videoList.length) {
+    return null;
+  }
+
+  return <Player list={videoList} />;
+}
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <View file="laney.json" />
+      </Route>
+      <Route path="/other">
+        <View file="other.json" />
+      </Route>
+      <Route path="/skip">
+        <View file="skip.json" />
+      </Route>
+      <Route path="/admin">
+        <Admin />
+      </Route>
+    </Switch>
+  );
+}
+
+/*
 function App() {
   const [videoList, setVideoList] = useState([]);
   useEffect(() => {
@@ -139,5 +257,6 @@ function App() {
 
   return <Player list={videoList} />;
 }
+*/
 
 export default App;
